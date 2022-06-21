60c0a450a940ef9774244a5cfa7424549eaffbd4
Gabriel Ekodo <ekodogabriel@outlook.com>
2022-06-21 17:09:41 +0300

    edited config file

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(expressLayouts);
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(cookieParser());
 
